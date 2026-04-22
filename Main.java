3ea84d87466f8c7e9e411695fb7dbba58d837298
Max <maxisaurus.prime@gmail.com>
2026-04-22 13:39:29 -0400

    testing

--- Main.java
+++ Main.java
@@ -44,7 +44,7 @@ public class Main {
 		
 		// Call setLevel() to change one OwnedPokemon's level, then call printInfo() again and confirm the stats changed
 		ownedVenusaur.setLevel(67);
-		ownedV enusaur.printInfo();
+		ownedVenusaur.printInfo();
 		
 		// Call setCurrentHp(0) on one OwnedPokemon and use isFainted() to confirm it returns true
 		ownedVenusaur.setCurrentHp(0);
